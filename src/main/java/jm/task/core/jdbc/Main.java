dd765cf88f7d45c3953731864ffa3ffa530dabb2
IIgorOs <105365567+IIgorOs@users.noreply.github.com>
2022-05-16 10:58:26 +0300

    Update Main.java

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,28 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+
+    static final String DB_URL = "jdbc:mysql://localhost:3306/mydb";
+    static final String USER = "root";
+    static final String PASS = "1102";
+
+    public static void main(String[] args)
+    {
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        System.out.println("Created db..");
+
+        userService.saveUser("Vitya", "First", (byte) 23);
+        userService.saveUser("Max", "Second", (byte) 22);
+        userService.saveUser("Oleg", "Third", (byte) 44);
+        userService.saveUser("Misha", "Last", (byte) 21);
+
+        System.out.println(userService.getAllUsers());
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
+
     }
 }
